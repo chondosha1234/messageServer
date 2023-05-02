cb0422c437ad6eab6473873da212cc309198e254
chondosha1234 <jmiller61193@gmail.com>
2023-05-02 00:30:11 -0400

    created 3 views for api endpoints

--- messageServer/views.py
+++ messageServer/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Message
+from .serializers import MessageSerializer
 
-# Create your views here.
+@api_view(['POST'])
+def send_message(request):
+    serializer = MessageSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET'])
+def get_messages(request, group_id):
+    messages = Message.objects.filter(group=group_id)
+    serializer = MessageSerializer(messages, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def get_group_list(request, user_id):
+    try:
+        user - User.objects.get(id=user_id)
+    except User.DoesNotExist:
+        return Response({'error': 'User does not exist'}, status = 404)
+
+    groups = user.group_list
+    serializer = GroupSerializer(groups, many=True)
+    return Response(serializer.data)
